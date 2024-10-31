c537db8dcee1caf47730801e7f9a402e426549a3
KS2013 <ksarker2013@gmail.com>
2024-10-31 20:52:16 +0000

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,14 @@
+#Print a statement
+print("Welcome to the world of coding")
+
+#Print a number 
+print(4)
+
+#Next line after print
+print("Hello Python \n")
+
+#Pass multiple arguments for printing
+print("Hello" , 5)
+
+#End argumet of Print Statement
+print("welcome to ", end = "*" )
---
+++ A2.py
@@ -0,0 +1,13 @@
+#Creating different variables
+x = 4
+y = 13
+
+print(x)
+print(y)
+
+codingal = "codingal"
+
+#User Input
+name = input("What is your name? \n")
+
+print("\nHello ",name,"\n Welcome to codingal.") 
